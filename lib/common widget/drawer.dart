309abaa04ddf76d65b9d75cda2aafaecf18cd56f
Ajay <devjc0011@gmail.com>
2023-01-09 14:52:17 +0530

    change

---
+++ lib/common widget/drawer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/config.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+
+import '../Screens/menu screen/menu_screen.dart';
+
+
+class Drawer extends StatelessWidget {
+   Drawer({Key? key}) : super(key: key);
+
+  final ZoomDrawerController drawerController = ZoomDrawerController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ZoomDrawer(
+      controller: drawerController,
+      menuScreen: const MenuScreen(),
+      mainScreen: this,
+      borderRadius: 24.0,
+      showShadow: true,
+      angle: 0.0,
+      menuScreenWidth: MediaQuery.of(context).size.width,
+      drawerShadowsBackgroundColor: Colors.grey.shade300,
+      slideWidth: MediaQuery.of(context).size.width * 0.62 ,
+    );
+  }
+}
